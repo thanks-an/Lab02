881cfa87c57d820a82ce206d2250b8dab8900c4c
thanks-an <dinhducthanhan100@gmail.com>
2024-10-22 17:12:07 +0700

    Add AimsProject to Lab02
    aimsproject dat dia phim, co chuc nang them/xoa dia dvd khoi gio hang, xong tinh tien

---
+++ Lab02/AimsProject/src/main/java/aimsproject/DigitalVideoDisc.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aimsproject;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DigitalVideoDisc {
+    private String title; 
+    private String category; 
+    private String director; 
+    private int length; 
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+    
+}
